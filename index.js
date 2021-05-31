4ee289edebafee8332d45da6a86dfad24b2b38ea
Aayush-Raj-Nepal <nepalaayush2404@gmial.com>
2021-05-31 16:49:13 +0545

    Uploading to server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let listener = app.listen(process.env.PORT || 4000, () => {
 app.enable("trust proxy")
 app.use(Express.json())
 app.use(cors())
-app.use("/",Express.static("Frontend"))
+app.use("/",Express.static("UI/frontend/build"))
 
 app.get('/printHello', (req, res) => {
   res.send('Hello World!')
@@ -19,5 +19,5 @@ app.get('/printHello', (req, res) => {
 
 require("./app/mongodb")
 
-import Router from "./routers"
-app.use("/", Router)
+// import Router from "./routers"
+// app.use("/", Router)
